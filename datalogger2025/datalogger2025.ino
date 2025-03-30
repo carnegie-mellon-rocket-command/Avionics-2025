545d2e18d8d9f6a40250f3d9273844c35a6a1c83
taterowney <51540870+taterowney@users.noreply.github.com>
2025-03-30 15:42:12 -0400

    New logger file name

--- datalogger2025/datalogger2025.ino
+++ datalogger2025/datalogger2025.ino
@@ -116,7 +116,7 @@ bool sd_active = false;
 #if SUBSCALE
     String file_name = "subscl_2.txt"; // ⚠⚠⚠ FILE NAME MUST BE 8 CHARACTERS OR LESS OR ARDUINO CANNOT WRITE IT (WHY?!?!) ⚠⚠⚠
 #else
-    String file_name = "fullsc_1.txt";
+    String file_name = "PDF.txt";
 #endif
 
 // SENSOR OBJECTS
